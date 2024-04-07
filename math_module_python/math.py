073caa7045d730138028276cc60c06cbcc9abc1a
amitsunil75 <amitsunil75@gmail.com>
2024-04-07 15:20:40 +0530

    math modeule

---
+++ math_module_python/math.py
@@ -0,0 +1,18 @@
+import math
+from math import sqrt,ceil,floor,factorial,fabs,copysign,gcd
+print(sqrt(4.01))
+# outout:200
+print(ceil(4.01))
+# output:5
+print(floor(4.01))
+# output:4
+print(factorial(3))
+# output:1*2*3=6
+print(fabs(-33))
+# output:absolute value of (-33) =33
+print(copysign(-2,3))
+# output :2
+print(copysign(2,-3))
+# output:-2
+print(gcd(4))
+# output:4

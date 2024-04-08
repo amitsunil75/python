199bab99608e4f2b48d1054cf852c4f7d65bfc0f
amitsunil75 <amitsunil75@gmail.com>
2024-04-08 07:01:03 +0530

    string  modeule

--- seperators/sep_python.py
+++ seperators/sep_python.py
@@ -5,3 +5,13 @@ print(12,"May",1997,sep="/")
 # output:12/May/1997
 print("h","e","l","l","0",end='*')
 # output:h e l l 0*
+
+tip=('05','04','2024')
+sep='/'
+tip= sep.join(tip)
+print('\n'+tip)
+# output:05/04/2024
+print('elephant'.rjust(12,'@'))
+# output:@@@@elephant
+print('elephant'.ljust(12,'@'))
+# output:elephant@@@@

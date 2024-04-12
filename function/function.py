012c2ebfcb3f7914f3c2595c1edad150f4151a5e
amitsunil75 <amitsunil75@gmail.com>
2024-04-13 00:30:29 +0530

    FUNCTIONS

--- function/function.py
+++ function/function.py
@@ -16,17 +16,68 @@ add(2,3)
 # output:0.5
 
 # factorial
-n= int(input('enter your number to find factorial:'))
-def fact(n):
-    Fact=1
-    for i in range(1,n+1):
-        Fact*=i
-    return Fact
-result=fact(n)
-print(f'Factorial of {n} is {result}')
+# n= int(input('enter your number to find factorial:'))
+# def fact(n):
+#     Fact=1
+#     for i in range(1,n+1):
+#         Fact*=i
+#     return Fact
+# result=fact(n)
+# print(f'Factorial of {n} is {result}')
 # OUTPUT:
 # enter your number to find factorial:6
 # Factorial of 6 is 720
 
+# ARBITARY ARGUMENT
+def arbitory(*args):
+    print(args)
 
+arbitory(2,3,4)
+# output:(2, 3, 4)
 
+# keyword argument
+def keywd(a,b):
+    print(a+b)
+keywd(a=2,b=2)
+
+# arbitory keyword Argument
+def aka(**args):
+    print(args)
+aka(a=2,b=3,c=4)
+# output:{'a': 2, 'b': 3, 'c': 4}
+def infom(**info):
+    print(f'roll no:{info['rollno']} ,age is {info['age']} and name is {info['name']}')
+infom(rollno=2,age=3,name='amit')
+infom(rollno=3,age=12,name='shifana')
+# output:
+# roll no:2 ,age is 3 and name is amit
+# roll no:3 ,age is 12 and name is shifana
+
+# default argument
+def examStatus(isFailed=True):
+    if isFailed:
+        print('failed')
+    else:
+        print('passed')
+examStatus()
+examStatus(False)
+# output:
+# failed
+# passed
+
+def result(x):
+    x=x/2
+    x=x+9
+    x=x*2
+    x=x-2
+    x=x+5
+    x=x**2
+    x=x*10
+    x=x+11
+    x=x+9
+    x=x%25
+    return 'finished' ,x
+if('finished'==result(2)[0]):
+    print(result(2))
+
+# output:('finished', 10.0)
--- problems/number_pattern.py
+++ problems/number_pattern.py
@@ -11,7 +11,7 @@
 #       else:
 #          print(' ',end='')
 #    print()      
-
+print('\n')
 for i in range(5):
    for j in range(4):
       if(i==0 and j in(0,1,2)) or (i==1 and j in(0,3)) or (i==2 and j in(0,1,2)) or (i==3 and j ==0) or   (i==4 and j ==0):
@@ -22,3 +22,48 @@ for i in range(5):
 
 # ltt='a23b56'
 # if(ltt.isnumeric()):
+
+print('\n its is P \n\n ')
+for i in range(5):
+   for j in range(5):
+      if(i==0 and j in (0,4)) or (i==1 and j in (1,3)) or (i==2 and j==2) or ((i==3 and j==2)) or (i==4 and j==2):
+         print('*',end='')
+      else:
+         print(' ',end='')
+   print()
+
+print('\n its is Y \n\n ')
+for i in range(5):
+   for j in range(5):
+      if(i==0 and j==2) or (i==1 and j in(1,3)) or (i==1 and j in(1,3)) or(i==2 and j in(0,1,2,3,4)) or (i==3 and j in(0,4)) or (i==4 and j in(0,4)):
+          print('*',end='')
+      else:
+         print(' ',end='')
+   print()
+
+print('\n its is A \n\n ')
+for i in range(7):
+   for j in range(7):
+      if(i==0 and j in (0,6)) or (i==1 and j in (0,1,5,6)) or (i==2 and j in (0,2,4,6)) or (i==3 and j in (0,3,6)) or (i==4 and j in (0,6))  :
+          print('*',end='')
+      else:
+         print(' ',end='')
+   print()
+print('\n its is M \n\n ')
+for i in range(5):
+   for j in range(5):
+      if(i==0 and j in (0,1,2,3,4)) or (i==1 and j==2) or (i==2 and j==2)  or (i==3 and j==2) or(i==4 and j in(0,1,2,3,4)) :
+          print('*',end='')
+      else:
+         print(' ',end='')
+   print()
+
+
+print('\n\n')
+for i in range(5):
+   for j in range(5):
+      if(i==0 and j in (0,1,2,3,4)) or (i==1 and j==2) or (i==2 and j==2)  or (i==3 and j==2)  :
+          print('*',end='')
+      else:
+         print(' ',end='')
+   print()
